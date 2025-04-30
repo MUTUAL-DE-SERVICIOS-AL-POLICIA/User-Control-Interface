25a5e863cc352305f564f874037f8de4b0bcac10
ferbautistamollo <ferbautistamollo@gmail.com>
2025-04-30 17:50:36 -0400

    INTERFAZ REDIRECCIÓN A LOS TABS PERMISOS

---
+++ src/app/[rol]/users/[uuid]/page.tsx
@@ -0,0 +1,15 @@
+import { redirect } from "next/navigation";
+
+import { getDataSidebar } from "@/api";
+
+interface Props {
+  params: { rol: string; uuid: string };
+}
+
+export default async function Page({ params }: Props) {
+  const { rol, uuid } = await params;
+
+  const { sidebar } = await getDataSidebar();
+
+  redirect(`/${rol}/users/${uuid}/${sidebar[0].shortened}`);
+}
