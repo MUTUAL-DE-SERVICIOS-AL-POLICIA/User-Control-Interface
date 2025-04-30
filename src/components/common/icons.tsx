f0da77ed086593817af8f77722323ab83bc22114
ferbautistamollo <ferbautistamollo@gmail.com>
2025-04-30 17:54:17 -0400

    ICONO NUEVO DE PARA BER Y PERMITIR USUARIOS

--- src/components/common/icons.tsx
+++ src/components/common/icons.tsx
@@ -236,3 +236,44 @@ export const ChevronDownIcon = ({ strokeWidth = 1.5, ...otherProps }) => {
     </svg>
   );
 };
+
+export const PassKeyFill = ({
+  width,
+  height,
+  className = "",
+  ...props
+}: IconSvgProps) => {
+  return (
+    <svg
+      className={`fill-black dark:fill-white ${className}`}
+      height={height || "24"}
+      viewBox="0 0 24 24"
+      width={width || "24"}
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path d="M9.496 2a5.25 5.25 0 0 0-2.519 9.857A9.006 9.006 0 0 0 .5 20.228a.751.751 0 0 0 .728.772h5.257c3.338.001 6.677.002 10.015 0a.5.5 0 0 0 .5-.5v-4.669a.95.95 0 0 0-.171-.551 9.02 9.02 0 0 0-4.814-3.423A5.25 5.25 0 0 0 9.496 2Z" />
+      <path d="M23.625 10.313c0 1.31-.672 2.464-1.691 3.134a.398.398 0 0 0-.184.33v.886a.372.372 0 0 1-.11.265l-.534.534a.188.188 0 0 0 0 .265l.534.534c.071.07.11.166.11.265v.347a.374.374 0 0 1-.11.265l-.534.534a.188.188 0 0 0 0 .265l.534.534a.37.37 0 0 1 .11.265v.431a.379.379 0 0 1-.097.253l-1.2 1.319a.781.781 0 0 1-1.156 0l-1.2-1.319a.379.379 0 0 1-.097-.253v-5.39a.398.398 0 0 0-.184-.33 3.75 3.75 0 1 1 5.809-3.134ZM21 9.75a1.125 1.125 0 1 0-2.25 0 1.125 1.125 0 0 0 2.25 0Z" />
+    </svg>
+  );
+};
+
+export const Usage = ({
+  width,
+  height,
+  className = "",
+  ...props
+}: IconSvgProps) => {
+  return (
+    <svg
+      className={`fill-black dark:fill-white ${className}`}
+      height={height || "24"}
+      viewBox="0 0 24 24"
+      width={width || "24"}
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path d="M3 6a1 1 0 0 1 1-1h5a1 1 0 0 1 1 1v5a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1Zm1.5 4.5h4v-4h-4Zm8.25-5a.75.75 0 0 0 0 1.5h7.5a.75.75 0 0 0 0-1.5h-7.5Zm0 6a.75.75 0 0 0 0 1.5h7.5a.75.75 0 0 0 0-1.5h-7.5Zm0 6a.75.75 0 0 0 0 1.5h7.5a.75.75 0 0 0 0-1.5h-7.5Zm-2.97-2.53a.75.75 0 0 1 0 1.06l-3.5 3.5a.75.75 0 0 1-1.06 0l-2-2a.75.75 0 1 1 1.06-1.06l1.47 1.47 2.97-2.97a.75.75 0 0 1 1.06 0Z" />
+    </svg>
+  );
+};
