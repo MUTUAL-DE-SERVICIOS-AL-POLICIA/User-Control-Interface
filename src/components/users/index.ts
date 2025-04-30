cbfac47aaf459a58aca96a6055f7d0701ebafd9f
ferbautistamollo <ferbautistamollo@gmail.com>
2025-04-30 17:54:41 -0400

    IMPORTACION DE NUEVOS COMPONENTES

--- src/components/users/index.ts
+++ src/components/users/index.ts
@@ -1,2 +1,6 @@
-export { TableUsers } from "./table";
-export { UserSidebar } from "./userSidebar";
+export { TableUsers } from "./tableUsers";
+export { UserInfo } from "./userInfo";
+export { TabsSidebar } from "./tabsSidebar";
+export { ModalLdap } from "./modalLdap";
+export { TableLdapUsers } from "./tableLdapUsers";
+export { ListBox } from "./listBox";
