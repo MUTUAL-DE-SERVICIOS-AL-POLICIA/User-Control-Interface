110c19ae61c42e8a9d900b300a1d10889efb6bf2
ferbautistamollo <ferbautistamollo@gmail.com>
2025-05-08 14:53:55 -0400

    RUTA DE PREUBA GET USER ROLES

--- src/api/users/users.ts
+++ src/api/users/users.ts
@@ -83,3 +83,164 @@ export async function getUserManagementModules(): Promise<ResponseData> {
     };
   }
 }
+
+export async function getUserRoles(
+  userUuid: string,
+  moduleId: string,
+): Promise<ResponseData> {
+  try {
+    // const response = await apiClient.GET(`auth/userManagementRoles`);
+    // const data = await response.json();
+
+    const data = [
+      {
+        id: 1,
+        name: "Plataforma",
+        state: true,
+      },
+      {
+        id: 2,
+        name: "Recepción",
+        state: false,
+      },
+      {
+        id: 3,
+        name: "Calificación",
+        state: true,
+      },
+      {
+        id: 4,
+        name: "Liquidación",
+        state: false,
+      },
+      {
+        id: 5,
+        name: "Legal",
+        state: false,
+      },
+      {
+        id: 6,
+        name: "Plataforma",
+        state: true,
+      },
+      {
+        id: 7,
+        name: "Recepción",
+        state: false,
+      },
+      {
+        id: 8,
+        name: "Calificación",
+        state: true,
+      },
+      {
+        id: 9,
+        name: "Liquidación",
+        state: false,
+      },
+      {
+        id: 10,
+        name: "Legal",
+        state: false,
+      },
+      {
+        id: 11,
+        name: "Plataforma",
+        state: true,
+      },
+      {
+        id: 12,
+        name: "Recepción",
+        state: false,
+      },
+      {
+        id: 13,
+        name: "Calificación",
+        state: true,
+      },
+      {
+        id: 14,
+        name: "Liquidación",
+        state: false,
+      },
+      {
+        id: 15,
+        name: "Legal",
+        state: false,
+      },
+      {
+        id: 16,
+        name: "Plataforma",
+        state: true,
+      },
+      {
+        id: 17,
+        name: "Recepción",
+        state: false,
+      },
+      {
+        id: 18,
+        name: "Calificación",
+        state: true,
+      },
+      {
+        id: 19,
+        name: "Liquidación",
+        state: false,
+      },
+      {
+        id: 20,
+        name: "Legal",
+        state: false,
+      },
+    ];
+
+    // if (!response.ok) {
+    //   return {
+    //     error: true,
+    //     message: "Error al obtener modulos",
+    //     data: response.statusText,
+    //   };
+    // }
+
+    return {
+      error: false,
+      message: "Datos de los modulos obtenidos exitosamente",
+      data,
+    };
+  } catch (err: any) {
+    return {
+      error: true,
+      message: "Error al obtener datos del usuario",
+      data: err.message,
+    };
+  }
+}
+
+export async function postUserRoles(
+  userUuid: string,
+  roleId: string,
+): Promise<ResponseData> {
+  try {
+    // const response = await apiClient.POST(`auth/${userUuid}/${roleId}`);
+
+    // if (!response.ok) {
+    //   return {
+    //     error: true,
+    //     message: "Error al asignar rol",
+    //     data: response.statusText,
+    //   };
+    // }
+
+    return {
+      error: false,
+      message: "Actualizado Exitosamente",
+    };
+  } catch (err: any) {
+    return {
+      error: true,
+      message: "Error al asignar rol",
+      data: err.message,
+    };
+  }
+}
