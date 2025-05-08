f75969a46b4a6b216369b2316fba9effb56067d9
ferbautistamollo <ferbautistamollo@gmail.com>
2025-05-08 15:21:53 -0400

    COMPONENTE LISTA PARA MOSTAR LOS MODULOS

--- src/components/users/listBox.tsx
+++ src/components/users/listBox.tsx
@@ -1,20 +1,81 @@
 "use client";
 
 import { Listbox, ListboxItem } from "@heroui/listbox";
+import { Divider } from "@heroui/divider";
+import { Spinner } from "@heroui/spinner";
+import { useState } from "react";
 
-import { userManagementModules } from "@/utils/interfaces";
+import { DrawerRoles } from "./drawerRoles";
+
+import { userManagementModules, userRoles } from "@/utils/interfaces";
+import { ChevronRightIcon } from "@/components/common";
+import { getUserRoles } from "@/api";
 
 interface Props {
   modules: userManagementModules[];
 }
+
 export const ListBox = ({ modules }: Props) => {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [userRoles, setUserRoles] = useState<userRoles[]>([]);
+  const [module, setModule] = useState<userManagementModules | null>(null);
+
+  const handleModuleClick = async (module: userManagementModules) => {
+    setLoading(true);
+    setModule(module);
+    try {
+      const response = await getUserRoles("", module.id);
+
+      setUserRoles(response.data);
+      setIsDrawerOpen(true);
+    } catch (error) {
+      console.error("Error al obtener roles:", error);
+      //toast
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
-    <Listbox
-      aria-label="Dynamic Actions"
-      items={modules}
-      onAction={(key) => alert(key)}
-    >
-      {(module) => <ListboxItem key={module.id}>{module.name}</ListboxItem>}
-    </Listbox>
+    <div className="border-small px-1 py-2 rounded-small border-default-200 dark:border-default-100 w-full">
+      <h4 className="px-1 py-1 text-large font-bold leading-none text-default-800 text-pretty ">
+        Módulos
+      </h4>
+      <Divider className="py-1" />
+      <Listbox aria-label="Actions">
+        {modules.map((module) => (
+          <ListboxItem
+            key={module.id}
+            color="primary"
+            endContent={
+              <div className="flex items-center gap-1 text-default-400">
+                <ChevronRightIcon className="text-xl font-bold" />
+              </div>
+            }
+            onPress={() => handleModuleClick(module)}
+          >
+            {module.name}
+          </ListboxItem>
+        ))}
+      </Listbox>
+
+      {loading && (
+        <div className="fixed top-0 left-0 w-full h-screen flex items-center justify-center bg-black/20 z-50">
+          <Spinner
+            classNames={{ label: "text-foreground mt-4" }}
+            size="lg"
+            variant="spinner"
+          />
+        </div>
+      )}
+
+      <DrawerRoles
+        isOpen={isDrawerOpen}
+        module={module}
+        userRoles={userRoles}
+        onClose={() => setIsDrawerOpen(false)}
+      />
+    </div>
   );
 };
