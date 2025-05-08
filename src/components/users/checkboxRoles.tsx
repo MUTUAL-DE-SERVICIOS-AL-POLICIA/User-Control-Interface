bde369a5f1edca83c62bbd622f0cb5e0cc507da6
ferbautistamollo <ferbautistamollo@gmail.com>
2025-05-08 15:02:46 -0400

    COMPONENTE NUEVO listado de roles checkbox PARA ASIGNACIÓN

---
+++ src/components/users/checkboxRoles.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import React, { useState } from "react";
+import { useParams } from "next/navigation";
+import { addToast } from "@heroui/toast";
+import { Spinner } from "@heroui/spinner";
+import { Checkbox } from "@heroui/checkbox";
+
+import { userRoles } from "@/utils/interfaces";
+import { postUserRoles } from "@/api";
+
+interface Props {
+  userRoles: userRoles[];
+  onClose: () => void;
+}
+export const CheckboxRoles = ({ userRoles, onClose }: Props) => {
+  const params = useParams();
+  const uuid = params?.uuid as string;
+  const [loading, setLoading] = useState(false);
+
+  const handleRoleChange = async (rol: userRoles) => {
+    setLoading(true);
+    try {
+      const { error, message } = await postUserRoles(uuid, rol.id);
+
+      if (!error) {
+        addToast({
+          title: "Aceptado",
+          description: `Rol ${rol.name} ${message}`,
+          color: "success",
+          timeout: 1000,
+          shouldShowTimeoutProgress: true,
+        });
+      } else {
+        addToast({
+          title: "Error",
+          description: `Rol ${rol.name} ${message}`,
+          color: "danger",
+          timeout: 1000,
+          shouldShowTimeoutProgress: true,
+        });
+        onClose();
+      }
+    } catch (error) {
+      console.error("Error al obtener roles:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <>
+      {userRoles.map((userRole) => (
+        <Checkbox
+          key={userRole.id}
+          defaultSelected={userRole.state}
+          value={String(userRole.id)}
+          onChange={() => {
+            handleRoleChange(userRole);
+          }}
+        >
+          {userRole.name}
+        </Checkbox>
+      ))}
+
+      {loading && (
+        <div className="fixed top-0 left-0 w-full h-screen flex items-center justify-center bg-black/20 z-50">
+          <Spinner
+            classNames={{ label: "text-foreground mt-4" }}
+            size="lg"
+            variant="spinner"
+          />
+        </div>
+      )}
+    </>
+  );
+};
